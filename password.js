4637c01b6a6483935124389926ed66d1ac69f4b7
Fantus <trent.shailer@gmail.com>
2021-01-03 21:35:10 +1300

    Adj Noun system

--- password.js
+++ password.js
@@ -7,6 +7,12 @@ const seed = require("seed-random");
 var text = fs.readFileSync("./words.txt").toString();
 var strings = text.split(/\r?\n/);
 
+var adjectivesRaw = fs.readFileSync("./adjective.txt").toString();
+var adjectiveStrings = adjectivesRaw.split(/\r?\n/);
+
+var nounsRaw = fs.readFileSync("./noun.txt").toString();
+var nounStrings = nounsRaw.split(/\r?\n/);
+
 function Generate(num, newSeed) {
 	var curSeed = new Date().getTime();
 	if (newSeed) curSeed = Number(newSeed);
@@ -15,16 +21,20 @@ function Generate(num, newSeed) {
 
 	var pw = "";
 
-	for (var i = 0; i < num; i++) {
-		var index = Math.floor(Math.random() * strings.length);
-		var word = strings[index];
-		if (i == 0) word = word[0].toUpperCase() + word.substr(1, word.length - 1);
-
-		pw += `${word} `;
+	for (var i = 0; i < num - 1; i++) {
+		pw += `${GetWord(adjectiveStrings, i == 0)} `;
 	}
-	pw = pw.substr(0, pw.length - 1);
-	pw += "-1";
+
+	pw += `${GetWord(nounStrings, num == 1)}-1`;
 	return { password: pw, length: pw.length, seed: curSeed };
+}
+
+function GetWord(list, capital = false) {
+	var index = Math.floor(Math.random() * list.length);
+	var word = list[index];
+	if (capital) word = word[0].toUpperCase() + word.substr(1, word.length - 1);
+
+	return word;
 }
 
 module.exports.Generate = Generate;
